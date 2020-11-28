e75833a62c8e68f20a71b33540dcc2d11ea641f1
niloyKGhosh <niloykumar256@gmail.com>
2020-11-27 20:08:46 -0500

    need to make winner function

--- src/index.ts
+++ src/index.ts
@@ -5,55 +5,75 @@ const boardElement = document.getElementById("board");
 const ROW_COUNT = 3;
 const COL_COUNT = 3;
 
-let boardState = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-];
-let currentMove = "X";
+type Cell = "X" | "O" | "";
 
-function createCell(row, col, content = "") {
-  const cell = document.createElement("button");
-  cell.setAttribute("data-row", row.toString());
-  cell.setAttribute("data-col", col.toString());
-  cell.setAttribute("data-content", content);
-  cell.classList.add("cell");
-  return cell;
+let boardState: [[Cell, Cell, Cell], [Cell, Cell, Cell], [Cell, Cell, Cell]] = [
+	["", "", ""],
+	["", "", ""],
+	["", "", ""],
+];
+
+let currentMove: "X" | "O" = "X";
+
+function createCell(row: number, col: number, content: Cell = "") {
+	const cell = document.createElement("button");
+	cell.setAttribute("data-row", row.toString());
+	cell.setAttribute("data-col", col.toString());
+	cell.setAttribute("data-content", content);
+	cell.classList.add("cell");
+
+	cell.addEventListener("click", () => {
+		if (winner !== "") {
+			return;
+		}
+
+		if (boardState[row][col] !== "") {
+			return;
+		}
+
+		boardState[row][col] = currentMove;
+		currentMove = currentMove === "X" ? "O" : "X";
+		renderBoard();
+	});
+	return cell;
 }
 
+function checkWinner() {
+	return "";
+}
 function renderBoard() {
-  if (!appElement) throw new Error("Cannot find app");
-  if (!boardElement) throw new Error("Cannot find board");
-  boardElement.innerHTML = "";
-  for (let i = 0; i < ROW_COUNT; i++) {
-    for (let j = 0; j < COL_COUNT; j++) {
-      boardElement.appendChild(createCell(i, j, boardState[i][j]));
-    }
-  }
-  const oldMoveElement = document.getElementById("move-element");
-  if (oldMoveElement) {
-    oldMoveElement.remove();
-  }
-  const moveElement = document.createElement("p");
-  moveElement.id = "move-element";
-  moveElement.innerText = `Next Move: ${currentMove}`;
-  moveElement.classList.add("current-move");
-  appElement.insertBefore(moveElement, document.getElementById("reset"));
+	if (!appElement) throw new Error("Cannot find app");
+	if (!boardElement) throw new Error("Cannot find board");
+	boardElement.innerHTML = "";
+	for (let i = 0; i < ROW_COUNT; i++) {
+		for (let j = 0; j < COL_COUNT; j++) {
+			boardElement.appendChild(createCell(i, j, boardState[i][j]));
+		}
+	}
+	const oldMoveElement = document.getElementById("move-element");
+	if (oldMoveElement) {
+		oldMoveElement.remove();
+	}
+	const moveElement = document.createElement("p");
+	moveElement.id = "move-element";
+	moveElement.innerText = `Next Move: ${currentMove}`;
+	moveElement.classList.add("current-move");
+	appElement.insertBefore(moveElement, document.getElementById("reset"));
 }
 
 function init() {
-  const resetButton = document.getElementById("reset");
-  if (!resetButton) throw new Error("No Reset button");
-  resetButton.addEventListener("click", () => {
-    boardState = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
-    ];
-    currentMove = "X";
-    renderBoard();
-  });
-  renderBoard();
+	const resetButton = document.getElementById("reset");
+	if (!resetButton) throw new Error("No Reset button");
+	resetButton.addEventListener("click", () => {
+		boardState = [
+			["", "", ""],
+			["", "", ""],
+			["", "", ""],
+		];
+		currentMove = "X";
+		renderBoard();
+	});
+	renderBoard();
 }
 
 init();
